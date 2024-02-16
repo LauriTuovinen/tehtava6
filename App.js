c7d405a613e6564c0467074709ab3f414d9ea56a
Lauri Tuovinen <lare.tuovinen@gmail.com>
2024-02-16 14:13:44 +0200

    tehtava6
    doned

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.heading}> Current weather </Text>
+      <Position />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '16'
+  }
 });
